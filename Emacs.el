f5c08d1348b83242240e429d048d0656fb76d615
linuxing3 <linuxing3@qq.com>
2021-11-12 17:42:26 -0400

    Regular commit on 2021-11-12 周五 17:42:26.21

--- Emacs.el
+++ Emacs.el
@@ -1,50 +1,39 @@
   (use-package lsp-ivy
-    :load-path "./localelpa/lsp-ivy"
+    ;;:load-path "./localelpa/lsp-ivy"
     :after lsp)
-
-  (use-package dap-mode
-    :load-path "./localelpa/dap-mode"
-    ;; Uncomment the config below if you want all UI panes to be hidden by default!
-    ;; :custom
-    ;; (lsp-enable-dap-auto-configure nil)
-    ;; :config
-    ;; (dap-ui-mode 1)
-    :commands dap-debug
-    :config
-    ;; Set up Node debugging
-    (require 'dap-node)
-    (dap-node-setup) ;; Automatically installs Node debug adapter if needed
-
-    ;; Bind `C-c l d` to `dap-hydra` for easy access
-    (general-define-key
-      :keymaps 'lsp-mode-map
-      :prefix lsp-keymap-prefix
-      "d" '(dap-hydra t :wk "debugger")))
-
-  (use-package typescript-mode
-    :mode "\\.ts\\'"
-    :hook (typescript-mode . lsp-deferred)
-    :config
-    (setq typescript-indent-level 2))
 
   (use-package python-mode
     :ensure t
     :hook (python-mode . lsp-deferred)
     :custom
     ;; NOTE: Set these if Python 3 is called "python3" on your system!
-    ;; (python-shell-interpreter "python3")
-    ;; (dap-python-executable "python3")
+    (python-shell-interpreter "python3")
+    (dap-python-executable "python3")
     (dap-python-debugger 'debugpy)
     :config
-    (require 'dap-python))
+    (require 'dap-python)
+    (dap-register-debug-template "My App"
+        (list :type "python"
+                :args "-i"
+                :cwd nil
+                :env '(("DEBUG" . "1"))
+                :target-module (expand-file-name "~/src/myapp/.env/bin/myapp")
+                :request "launch"
+                :name "My App")))
 
   (use-package company
-    :after lsp-mode
-    :hook (lsp-mode . company-mode)
+    ;; :after lsp-mode
+    ;; :hook (lsp-mode . company-mode)
     :bind (:map company-active-map
-           ("<tab>" . company-complete-selection))
+           ("TAB" . company-complete-selection)
+           ("<tab>" . company-complete-selection)
+           ("S-TAB" . company-complete-select-previous)
+           ("<backtab>" . company-complete-select-previous))
           (:map lsp-mode-map
            ("<tab>" . company-indent-or-complete-common))
+    :config
+    (global-company-mode 1)
+    (define-key company-mode-map [remap indent-for-tab-command] 'company-indent-for-tab-command)
     :custom
     (company-minimum-prefix-length 1)
     (company-idle-delay 0.0))
