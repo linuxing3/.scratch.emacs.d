ee7d5966046a0eda6856654e905d5bdb47df2b9d
linuxing3 <linuxing3@qq.com>
2021-11-17 12:32:06 -0400

    Regular commit on 2021-11-17 周三 12:32:06.49

--- init.el
+++ init.el
@@ -92,11 +92,15 @@
 
   ;; Make modern look
   (show-paren-mode  1)
-  (scroll-bar-mode -1)        ; Disable visible scrollbar
   (tool-bar-mode -1)          ; Disable the toolbar
   (tooltip-mode -1)           ; Disable tooltips
-  (set-fringe-mode 10)        ; Give some breathing room
   (menu-bar-mode -1)            ; Disable the menu bar
+
+  (when (display-graphic-p)
+    (progn
+        (scroll-bar-mode -1)        ; Disable visible scrollbar
+        (set-fringe-mode 10)        ; Give some breathing room
+      ))
 
   ;; Set up the visible bell
   (setq visible-bell t)
@@ -147,7 +151,7 @@
      (cond
       ((member "WenQuanYi Micro Hei" (font-family-list)) "WenQuanYi Micro Hei"))))))
 
-(+modern-ui-chinese-h)
+(when (display-graphic-p) (+modern-ui-chinese-h))
 
   ;; Make ESC quit prompts
   (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
