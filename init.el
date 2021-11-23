81dc6cd326f06d33e1b905f0538a92326f92b622
linuxing3 <linuxing3@qq.com>
2021-11-23 07:56:36 -0500

    Commited by vagrant from /home/vagrant/.scratch.emacs.d on 2021-11-23-07:56

--- init.el
+++ init.el
@@ -41,9 +41,9 @@
 ;;                          ("elpa" . "https://elpa.gnu.org/packages/")))
 ;; ------ 腾讯源 ------
 (setq package-archives '(("gnu"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
-  		   ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")
-  		   ("melpa-stable" . "http://mirrors.cloud.tencent.com/elpa/melpa-stable/")
-  		   ("org" . "http://mirrors.cloud.tencent.com/elpa/org/")))
+  		       ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")
+  		       ("melpa-stable" . "http://mirrors.cloud.tencent.com/elpa/melpa-stable/")
+  		       ("org" . "http://mirrors.cloud.tencent.com/elpa/org/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -123,10 +123,10 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(set-face-attribute 'default nil :font "Source Code Pro" :height efs/default-font-size)
+(set-face-attribute 'default nil :font "BlexMono NF" :height efs/default-font-size)
 
 ;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Source Code Pro" :height efs/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "BlexMono NF" :height efs/default-font-size)
 
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height efs/default-variable-font-size :weight 'regular)
@@ -756,3 +756,16 @@
 
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(simple-httpd evil-org ox-hugo org-journal org-pomodoro elfeed-org ox-reveal org-brain org-download htmlize)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
